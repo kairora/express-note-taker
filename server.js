157abb0285957eee207f5b7e4d0218e4de825519
Brianna Bullock <brianna.bullock16@gmail.com>
2020-07-23 00:07:48 -0500

    set up express app to handle parsing the data

--- server.js
+++ server.js
@@ -5,3 +5,10 @@ const app = express();
 // saves port to a variable
 const PORT = process.env.PORT || 8080;
 
+// express app handles data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
