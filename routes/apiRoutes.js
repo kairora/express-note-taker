8c5366da5f3a0b452598f9fb66f720eb7c2eb2b9
Brianna Bullock <brianna.bullock16@gmail.com>
2020-07-25 00:13:31 -0500

    adds the new note to the db and saves

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,24 +2,35 @@ const fs = require("fs")
 let myDB = require("../db/db.json")
 const path = require("path")
 
+
+
+
 // export
 module.exports = function(app) {
 
     const readDB =  JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), (err, data) => {
             if (err) throw err;
-            // const content = JSON.parse(data);
         }));
  
     // gets the correct data from the database when user visits the route
     app.get("/api/notes", function(req, res) {
        return res.json(readDB);
+       console.log(typeof readDB)
     })
 
-    // app.post("/api/notes", function(req, res) {
-    //     let userNote = req.body;
-    //     readDB.push(userNote)
+    const writeUserNote = function(note) {
+        fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(note), (err, data) => {
+        if (err) throw err;
+        })
+    }
 
-    //  })
+    app.post("/api/notes", function(req, res) {
+        let userNote = req.body;
+        readDB.push(userNote);
+        writeUserNote(readDB);
+        return res.json(readDB)
+
+     })
 
 
 
